610446f28bf1cbf7ce624f3a41134c7a43571cfc
defi-dev <defi.dev@protonmail.com>
2022-03-15 20:32:07 +0800

    TornPowerIndexConnector natspec

--- contracts/connectors/TornPowerIndexConnector.sol
+++ contracts/connectors/TornPowerIndexConnector.sol
@@ -43,6 +43,12 @@ contract TornPowerIndexConnector is AbstractStakeRedeemConnector {
     return accumulatedRewardPerTorn().sub(accumulatedRewardRateOnLastUpdate());
   }
 
+  /**
+   * @notice Calculate pending rewards of TornStaking
+   * @param _accRewardPerTorn TornStaking variable, getting by accumulatedRewardPerTorn()
+   * @param _accRewardRateOnLastUpdate TornStaking variable, getting by accumulatedRewardRateOnLastUpdate()
+   * @param _lockedBalance Staked amount in TornGovernance
+   */
   function pendingReward(
     uint256 _accRewardPerTorn,
     uint256 _accRewardRateOnLastUpdate,
@@ -54,6 +60,14 @@ contract TornPowerIndexConnector is AbstractStakeRedeemConnector {
       .add(accumulatedReward());
   }
 
+  /**
+   * @notice Calculate forecast rewards from TornStaking
+   * @param _accRewardPerTorn TornStaking variable, getting by accumulatedRewardPerTorn()
+   * @param _accRewardRateOnLastUpdate TornStaking variable, getting by accumulatedRewardRateOnLastUpdate()
+   * @param _reinvestDuration Duration in seconds to forecast future rewards
+   * @param _lastRewardsUpdate Last stake/unstake/claim action timestamp
+   * @param _lockedBalance Staked amount in TornGovernance
+   */
   function forecastReward(
     uint256 _accRewardPerTorn,
     uint256 _accRewardRateOnLastUpdate,
@@ -68,6 +82,12 @@ contract TornPowerIndexConnector is AbstractStakeRedeemConnector {
       .div(RATIO_CONSTANT);
   }
 
+  /**
+   * @notice Calculate pending rewards from TornStaking and forecast
+   * @param _lastClaimRewardsAt Last claim action timestamp
+   * @param _lastChangeStakeAt Last stake/unstake action timestamp
+   * @param _reinvestDuration Duration to forecast future rewards, based on last stake/unstake period of rewards
+   */
   function getPendingAndForecastReward(
     uint256 _lastClaimRewardsAt,
     uint256 _lastChangeStakeAt,
@@ -84,6 +104,103 @@ contract TornPowerIndexConnector is AbstractStakeRedeemConnector {
       forecastReward(accRewardPerTorn, accRewardOnLastUpdate, _reinvestDuration, lastUpdate, lockedBalance),
       forecastReward(accRewardPerTorn, accRewardOnLastUpdate, _reinvestDuration, lastUpdate, pending)
     );
+  }
+
+  /**
+   * @notice Checking: is pending rewards in TORN enough to cover transaction cost to reinvest
+   * @param _claimParams Claim parameters, that stored in PowerIndexRouter
+   * @param _lastClaimRewardsAt Last claim action timestamp
+   * @param _lastChangeStakeAt Last stake/unstake action timestamp
+   */
+  function isClaimAvailable(
+    bytes calldata _claimParams,
+    uint256 _lastClaimRewardsAt,
+    uint256 _lastChangeStakeAt
+  ) external view virtual override returns (bool) {
+    (uint256 paybackDuration, uint256 gasToReinvest) = unpackClaimParams(_claimParams);
+    (, , uint256 forecastByPending) = getPendingAndForecastReward(_lastClaimRewardsAt, _lastChangeStakeAt, paybackDuration);
+    return forecastByPending >= getTornUsedToReinvest(gasToReinvest, tx.gasprice);
+  }
+
+  /**
+   * @notice Get reinvest transaction cost in TORN
+   * @param _gasUsed Gas used for reinvest transaction
+   * @param _gasPrice Gas price
+   */
+  function getTornUsedToReinvest(uint256 _gasUsed, uint256 _gasPrice) public view returns(uint256) {
+    return calcTornOutByWethInWithRatio(gasToReinvest.mul(_gasPrice), getTornPriceRatio());
+  }
+
+  /**
+   * @notice Get Uniswap V3 TORN price ratio
+   */
+  function getTornPriceRatio() public view returns (uint256) {
+    uint32 uniswapTimePeriod = 5400;
+    uint24 uniswapTornSwappingFee = 10000;
+    uint24 uniswapWethSwappingFee = 0;
+
+    return UniswapV3OracleHelper.getPriceRatioOfTokens(
+      [address(UNDERLYING), UniswapV3OracleHelper.WETH],
+      [uniswapTornSwappingFee, uniswapWethSwappingFee],
+      uniswapTimePeriod
+    );
+  }
+
+  /**
+   * @notice Convert TORN amount to WETH amount with built in ratio
+   * @param _tornAmount TORN amount to convert
+   */
+  function calcWethOutByTornIn(uint256 _tornAmountIn) external view returns (uint256) {
+    return calcWethOutByTornInWithRatio(_tornAmountIn, getTornPriceRatio());
+  }
+
+  /**
+   * @notice Convert TORN amount to WETH amount by provided rario
+   * @param _tornAmount TORN amount to convert
+   * @param _ratio Uniswap V3 ratio
+   */
+  function calcWethOutByTornInWithRatio(uint256 _tornAmount, uint256 _ratio) public pure returns (uint256) {
+    return _tornAmount.mul(UniswapV3OracleHelper.RATIO_DIVIDER).div(_ratio);
+  }
+
+  /**
+   * @notice Convert WETH amount to TORN amount with built in ratio
+   * @param _wethAmount WETH amount to convert
+   */
+  function calcTornOutByWethIn(uint256 _wethAmount) external view returns (uint256) {
+    return calcTornOutByWethInWithRatio(_wethAmount, getTornPriceRatio());
+  }
+
+  /**
+   * @notice Convert WETH amount to TORN amount with provided ratio
+   * @param _wethAmount WETH amount to convert
+   * @param _ratio Uniswap V3 ratio
+   */
+  function calcTornOutByWethInWithRatio(uint256 _wethAmount, uint256 _ratio) public pure returns (uint256) {
+    return _wethAmount.mul(_ratio).div(UniswapV3OracleHelper.RATIO_DIVIDER);
+  }
+
+  /**
+   * @notice Pack claim params to bytes.
+   */
+  function packClaimParams(
+    uint256 paybackDuration,
+    uint256 gasToReinvest
+  ) public pure returns (bytes memory) {
+    return abi.encode(paybackDuration, gasToReinvest);
+  }
+
+  /**
+   * @notice Unpack claim params from bytes to variables.
+   */
+  function unpackClaimParams(bytes memory _claimParams) public pure returns (
+    uint256 paybackDuration,
+    uint256 gasToReinvest
+  ) {
+    if (_claimParams.length == 0 || keccak256(_claimParams) == keccak256("")) {
+      return (0, 0);
+    }
+    (paybackDuration, gasToReinvest) = abi.decode(_claimParams, (uint256, uint256));
   }
 
   /*** OVERRIDES ***/
@@ -109,66 +226,5 @@ contract TornPowerIndexConnector is AbstractStakeRedeemConnector {
 
   function _redeemImpl(uint256 _amount) internal override {
     _callExternal(PI_TOKEN, GOVERNANCE, ITornGovernance.unlock.selector, abi.encode(_amount));
-  }
-
-  function isClaimAvailable(
-    bytes calldata _claimParams,
-    uint256 _lastClaimRewardsAt,
-    uint256 _lastChangeStakeAt
-  ) external view virtual override returns (bool) {
-    (uint256 paybackDuration, uint256 gasToReinvest) = unpackClaimParams(_claimParams);
-    (, , uint256 forecastByPending) = getPendingAndForecastReward(_lastClaimRewardsAt, _lastChangeStakeAt, paybackDuration);
-    return forecastByPending >= getTornUsedToReinvest(gasToReinvest, tx.gasprice);
-  }
-
-  function getTornUsedToReinvest(uint256 gasUsed, uint256 gasPrice) public view returns(uint256) {
-    return calcWethToTornWithRatio(gasToReinvest.mul(gasPrice), getTornPriceRatio());
-  }
-
-  function getTornPriceRatio() public view returns (uint256) {
-    uint32 uniswapTimePeriod = 5400;
-    uint24 uniswapTornSwappingFee = 10000;
-    uint24 uniswapWethSwappingFee = 0;
-
-    return UniswapV3OracleHelper.getPriceRatioOfTokens(
-      [address(UNDERLYING), UniswapV3OracleHelper.WETH],
-      [uniswapTornSwappingFee, uniswapWethSwappingFee],
-      uniswapTimePeriod
-    );
-  }
-
-  function calcWethOutByTornIn(uint256 _tornAmountIn) public view returns (uint256) {
-    return calcTornToWethWithRatio(_tornAmountIn, getTornPriceRatio());
-  }
-
-  function calcTornToWethWithRatio(uint256 _tornAmount, uint256 _ratio) public pure returns (uint256) {
-    return _tornAmount.mul(UniswapV3OracleHelper.RATIO_DIVIDER).div(_ratio);
-  }
-
-  function calcWethToTornWithRatio(uint256 _wethAmount, uint256 _ratio) public pure returns (uint256) {
-    return _wethAmount.mul(_ratio).div(UniswapV3OracleHelper.RATIO_DIVIDER);
-  }
-
-  /**
-   * @notice Pack claim params to bytes.
-   */
-  function packClaimParams(
-    uint256 paybackDuration,
-    uint256 gasToReinvest
-  ) public pure returns (bytes memory) {
-    return abi.encode(paybackDuration, gasToReinvest);
-  }
-
-  /**
-   * @notice Unpack claim params from bytes to variables.
-   */
-  function unpackClaimParams(bytes memory _claimParams) public pure returns (
-    uint256 paybackDuration,
-    uint256 gasToReinvest
-  ) {
-    if (_claimParams.length == 0 || keccak256(_claimParams) == keccak256("")) {
-      return (0, 0);
-    }
-    (paybackDuration, gasToReinvest) = abi.decode(_claimParams, (uint256, uint256));
   }
 }
