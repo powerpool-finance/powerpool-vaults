1cf5e59539319fbcf4f252da6ccfdf5127c37f40
defi-dev <defi.dev@protonmail.com>
2022-04-11 12:16:45 +0800

    remove excess import from WrappedPiErc20.sol

--- contracts/WrappedPiErc20.sol
+++ contracts/WrappedPiErc20.sol
@@ -6,7 +6,6 @@ pragma experimental ABIEncoderV2;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
 import "./interfaces/PowerIndexNaiveRouterInterface.sol";
 import "./interfaces/PowerIndexRouterInterface.sol";
