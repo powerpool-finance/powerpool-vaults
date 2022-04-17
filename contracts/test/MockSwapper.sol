a285952b8fb343510df39084379528b87631425e
Poli Paul <polipaul777@hotmail.com>
2022-04-17 08:28:49 +0800

    Remove console.logs from a mock to make test output clear

--- contracts/test/MockSwapper.sol
+++ contracts/test/MockSwapper.sol
@@ -26,10 +26,7 @@ contract MockSwapper {
     address _tokenTo,
     uint256 _tokenFromAmount
   ) public {
-    console.log("swap", ratio[_tokenFrom][_tokenTo]);
     IERC20(_tokenFrom).transferFrom(msg.sender, address(this), _tokenFromAmount);
-    console.log("IERC20(_tokenTo).transfer ", _tokenFromAmount.mul(ratio[_tokenFrom][_tokenTo]).div(1 ether));
-    console.log("IERC20(_tokenTo).balanceOf", IERC20(_tokenTo).balanceOf(address(this)));
     IERC20(_tokenTo).transfer(msg.sender, _tokenFromAmount.mul(ratio[_tokenFrom][_tokenTo]).div(1 ether));
   }
 }
